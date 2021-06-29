a8970e8471017877562797b3ebe67cc1bb22ef0f
LalaNurjanah <lalanurjanah31@gmail.com>
2021-06-29 21:07:10 +0800

    Commit Pertemuan 9

--- src/views/friends/Home.vue
+++ src/views/friends/Home.vue
@@ -6,6 +6,9 @@
     <router-link class="btn btn-primary" to="/createfriends"
       >Add friends</router-link
     >
+
+<Cardfriends :friends="friends"/>
+
     <table class="table">
       <thead>
         <tr>
@@ -36,28 +39,30 @@
 import axios from 'axios'
 // @ is an alias to /src
 import Slider from "@/components/Slider.vue";
+import Cardfriends from "@/components/Cardfriends.vue";
 import { onMounted, ref } from 'vue';
 
 export default {
   name: "Home",
   components: {
     Slider,
+    Cardfriends,
   },
   setup(){
     let friends = ref([])
 
     onMounted(() => {
-      axios.get('http://pia.labirin.co.id/api/friends')
+      axios.get('http://127.0.0.1:8000/api/friends')
       .then(response => {
         friends.value = response.data.data
       })
       .catch(error =>{
         console.log(error)
-      })
-    })
+      });
+    });
 
   function friendDelete(id){
-    axios.delete(`http://pia.labirin.co.id/api/friends/${id}`)
+    axios.delete(`http://127.0.0.1:8000/api/friends/${id}`)
     .then(()=>{
       let z = this.friends.map(friends => friends.id).indexOf(id);
       this.friends.splice(z, 1)
